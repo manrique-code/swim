f4cd0dd6f56eb835e9c5d24d7b13d9050bc0b382
manrique-code <sergio.mnrq1@gmail.com>
2022-03-18 04:12:14 -0600

    Configuración y creación de la db
    * Establecemos los parámetros de la base de datos
    * Se crea la instancia y la base de datos en la app

---
+++ src/dao/db.js
@@ -0,0 +1,8 @@
+const nombre = "SWIMDB";
+const version = "1.0";
+const descripcion = "Base de datos de SWIM app.";
+const size = 50 * 1024 * 1024;
+
+let db = window.openDatabase(nombre, version, descripcion, size);
+
+export default db;
